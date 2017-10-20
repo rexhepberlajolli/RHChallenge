e76905212aa0966785f52518f538a3d4ab4caf63
rexhepberlajolli <rexhepberlajolli@gmail.com>
2017-10-20 20:40:01 +0200

    Add LoginPage View and Add login required decorator at HomePage

--- app/user_administration/views.py
+++ app/user_administration/views.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import HttpResponse
-from django.views.generic import View
+from django.views.generic import TemplateView, View
 import logging
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.shortcuts import HttpResponse
 
 logger = logging.getLogger(__name__)
 
 
+class LoginPage(TemplateView):
+    template_name = 'user_administration/login.html'
+
+
+@method_decorator(login_required, name='dispatch')
 class HomePage(View):
     def get(self, request):
-        logger.info("Home Page")
         return HttpResponse("Home Page")
