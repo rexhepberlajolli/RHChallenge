3898a3bba45bf72b108937fb4ff4c1dd8c3d9acd
rexhepberlajolli <rexhepberlajolli@gmail.com>
2017-10-20 20:37:59 +0200

    Add login required test case

--- app/user_administration/tests.py
+++ app/user_administration/tests.py
@@ -3,9 +3,16 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 
-class InitialTest(TestCase):
-    def setUp(self):
-        self.value = 1 + 1
-
-    def test_initial(self):
-        self.assertEqual(1 + 1, self.value)
+class LoginRequiredTest(TestCase):
+    def test_login_required(self):
+        response = self.client.get('/')
+        self.assertEqual(
+            response.status_code,
+            302,
+            msg="Login Required Validation Failed, Received code {0} instead of 302".format(response.status_code)
+        )
+        self.assertEqual(
+            response.url,
+            '/login?next=/',
+            msg="Login Required Redirection Failed, Received url {0} instead of /login?next=/".format(response.url)
+        )
