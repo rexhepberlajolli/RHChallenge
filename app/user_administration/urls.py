980f9abc5b95c9f9ed089b13e9538173bcac0952
rexhepberlajolli <rexhepberlajolli@gmail.com>
2017-10-20 20:40:22 +0200

    Add Login & Logout Routing

--- app/user_administration/urls.py
+++ app/user_administration/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from .views import HomePage
+from .views import HomePage, LoginPage
+from django.contrib.auth.views import logout_then_login
 
 urlpatterns = [
-    url(r'^', HomePage.as_view(), name='HomePage'),
+    url(r'^$', HomePage.as_view(), name='HomePage'),
+    url(r'^login/', LoginPage.as_view(), name='LoginPage'),
+    url(r'^logout/', logout_then_login, name='LoginPage'),
 ]
