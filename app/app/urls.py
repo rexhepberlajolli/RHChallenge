0c9036e56d6bf19adb8b195ee3bfb635a701c39b
rexhepberlajolli <rexhepberlajolli@gmail.com>
2017-10-20 19:14:28 +0200

    Add oauth routing

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 
 urlpatterns = [
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
     url(r'^', include('user_administration.urls', namespace="UserAdministration")),
 ]
