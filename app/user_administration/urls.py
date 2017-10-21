e20c4bcc2994a9bd1031aca71b2e8ba7b69558f7
rexhepberlajolli <rexhepberlajolli@gmail.com>
2017-10-21 15:54:25 +0200

    Add ClientDetailView Routing

--- app/user_administration/urls.py
+++ app/user_administration/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from .views import LoginPage, ClientCreateView, ClientListView
+from .views import LoginPage, ClientCreateView, ClientListView, ClientDetailView
 from django.contrib.auth.views import logout_then_login
 
 urlpatterns = [
     url(r'^$', ClientListView.as_view(), name='ClientListView'),
     url(r'^add$', ClientCreateView.as_view(), name='ClientCreateView'),
+    url(r'^clients/(?P<pk>[^/]+)/', ClientDetailView.as_view(), name='ClientDetailView'),
     url(r'^login/', LoginPage.as_view(), name='LoginPage'),
     url(r'^logout/', logout_then_login, name='LogoutPage'),
 ]
