edc8c8bdebe11e928511646a613a152620d5b215
rexhepberlajolli <rexhepberlajolli@gmail.com>
2017-10-21 17:35:09 +0200

    Add TestCase for ClientUpdateView

--- app/user_administration/tests.py
+++ app/user_administration/tests.py
@@ -77,3 +77,63 @@ class ClientDetailViewTest(LoginSetup):
             msg="Get client details failed, received client name {0} instead of {1}"
                 .format(client.first_name, self.custom_client.first_name)
         )
+
+
+class ClientUpdateViewTest(LoginSetup):
+    changed_data = {
+        'first_name': 'editedName',
+        'last_name': 'editedLastname',
+        'iban': 'XK051506001004471930'
+    }
+
+    def setUp(self):
+        super(ClientUpdateViewTest, self).setUp()
+        self.second_user = User.objects.create(username='testUser2', is_active=True, is_superuser=True)
+        self.second_user.set_password('RHChallenge')
+        self.second_user.save()
+        self.first_c = Clients.objects.create(first_name='RH', last_name='CH', iban='IBAN', createdBy=self.user)
+        self.second_c = Clients.objects.create(first_name='RH', last_name='CH', iban='IBAN', createdBy=self.second_user)
+
+    def test_update_client(self):
+        self.client.post('/clients/{0}/edit'.format(self.first_c.pk), data=self.changed_data)
+        first_client = Clients.objects.first()
+        self.assertEqual(
+            first_client.first_name,
+            self.changed_data.get('first_name'),
+            msg="Update client details failed, received first name {0} instead of {1}"
+                .format(first_client.first_name, self.changed_data.get('first_name'))
+        )
+        self.assertEqual(
+            first_client.last_name,
+            self.changed_data.get('last_name'),
+            msg="Update client details failed, received last name {0} instead of {1}"
+                .format(first_client.last_name, self.changed_data.get('last_name'))
+        )
+        self.assertEqual(
+            first_client.iban,
+            self.changed_data.get('iban'),
+            msg="Update client details failed, received iban {0} instead of {1}"
+                .format(first_client.iban, self.changed_data.get('iban'))
+        )
+
+    def test_update_forbidden_client(self):
+        self.client.post('clients/{0}/edit'.format(self.second_c.pk), data=self.changed_data)
+        second_client = Clients.objects.last()
+        self.assertEqual(
+            second_client.first_name,
+            self.second_c.first_name,
+            msg="Updating a forbidden user succeeded, received first name {0} instead of {1}"
+                .format(second_client.first_name, self.second_c.first_name)
+        )
+        self.assertEqual(
+            second_client.last_name,
+            self.second_c.last_name,
+            msg="Updating a forbidden user succeeded, received last name {0} instead of {1}"
+                .format(second_client.last_name, self.second_c.last_name)
+        )
+        self.assertEqual(
+            second_client.iban,
+            self.second_c.iban,
+            msg="Updating a forbidden user succeeded, received iban {0} instead of {1}"
+                .format(second_client.iban, self.second_c.iban)
+        )
