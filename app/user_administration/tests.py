ee7f1cbac198adeb8212c2078a1c01de999a72d7
rexhepberlajolli <rexhepberlajolli@gmail.com>
2017-10-21 18:52:38 +0200

    Improve Test Cases

--- app/user_administration/tests.py
+++ app/user_administration/tests.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import Clients
+from django.urls import reverse_lazy
 
 
 class LoginRequiredTest(TestCase):
@@ -32,29 +33,10 @@ class ClientListViewTest(LoginSetup):
     def setUp(self):
         super(ClientListViewTest, self).setUp()
         self.custom_client = Clients.objects.create(first_name='RH', last_name='CH', iban='IBAN')
-
-    def test_client_create(self):
-        data = {'first_name': 'Rexhep', 'last_name': 'Berlajolli', 'iban': 'XK051506001004471930'}
-        self.client.post('/add', data=data)
-        clients_count = Clients.objects.count()
-        self.assertEqual(
-            clients_count,
-            2,
-            msg="Create client failed, received {0} clients instead of 2".format(clients_count)
-        )
-
-    def test_client_create_validation(self):
-        data = {'first_name': 'Invalid', 'last_name': 'Data', 'iban': 'INVALID_IBAN'}
-        self.client.post('/add', data=data)
-        clients_count = Clients.objects.count()
-        self.assertEqual(
-            clients_count,
-            1,
-            msg="Insertion of invalid data succeeded, received {0} clients instead of 1".format(clients_count)
-        )
+        self.url = reverse_lazy('UserAdministration:ClientListView')
 
     def test_get_clients(self):
-        response = self.client.get('/')
+        response = self.client.get(self.url)
         clients = response.context_data['clients']
         self.assertEqual(
             list(clients),
@@ -63,13 +45,40 @@ class ClientListViewTest(LoginSetup):
         )
 
 
+class ClientCreateViewTest(LoginSetup):
+    def setUp(self):
+        super(ClientCreateViewTest, self).setUp()
+        self.url = reverse_lazy('UserAdministration:ClientCreateView')
+
+    def test_client_create(self):
+        data = {'first_name': 'Rexhep', 'last_name': 'Berlajolli', 'iban': 'XK051506001004471930'}
+        self.client.post(self.url, data=data)
+        clients_count = Clients.objects.count()
+        self.assertEqual(
+            clients_count,
+            1,
+            msg="Create client failed, received {0} clients instead of 2".format(clients_count)
+        )
+
+    def test_client_create_validation(self):
+        data = {'first_name': 'Invalid', 'last_name': 'Data', 'iban': 'INVALID_IBAN'}
+        self.client.post(self.url, data=data)
+        clients_count = Clients.objects.count()
+        self.assertEqual(
+            clients_count,
+            0,
+            msg="Insertion of invalid data succeeded, received {0} clients instead of 1".format(clients_count)
+        )
+
+
 class ClientDetailViewTest(LoginSetup):
     def setUp(self):
         super(ClientDetailViewTest, self).setUp()
         self.custom_client = Clients.objects.create(first_name='RH', last_name='CH', iban='IBAN')
+        self.url = reverse_lazy('UserAdministration:ClientDetailView', kwargs={'pk': self.custom_client.pk})
 
     def test_get_client_details(self):
-        response = self.client.get('/clients/{0}'.format(self.custom_client.pk), follow=True)
+        response = self.client.get(self.url, follow=True)
         client = response.context_data['client']
         self.assertEqual(
             client.first_name,
@@ -93,9 +102,11 @@ class ClientUpdateViewTest(LoginSetup):
         self.second_user.save()
         self.first_c = Clients.objects.create(first_name='RH', last_name='CH', iban='IBAN', createdBy=self.user)
         self.second_c = Clients.objects.create(first_name='RH', last_name='CH', iban='IBAN', createdBy=self.second_user)
+        self.first_url = reverse_lazy('UserAdministration:ClientUpdateView', kwargs={'pk': self.first_c.pk})
+        self.second_url = reverse_lazy('UserAdministration:ClientUpdateView', kwargs={'pk': self.second_c.pk})
 
     def test_update_client(self):
-        self.client.post('/clients/{0}/edit'.format(self.first_c.pk), data=self.changed_data)
+        self.client.post(self.first_url, data=self.changed_data)
         first_client = Clients.objects.first()
         self.assertEqual(
             first_client.first_name,
@@ -117,7 +128,7 @@ class ClientUpdateViewTest(LoginSetup):
         )
 
     def test_update_forbidden_client(self):
-        self.client.post('clients/{0}/edit'.format(self.second_c.pk), data=self.changed_data)
+        self.client.post(self.second_url, data=self.changed_data)
         second_client = Clients.objects.last()
         self.assertEqual(
             second_client.first_name,
@@ -143,11 +154,12 @@ class ClientDeleteViewTest(LoginSetup):
     def setUp(self):
         super(ClientDeleteViewTest, self).setUp()
         self.first_c = Clients.objects.create(first_name='RH', last_name='CH', iban='IBAN')
+        self.url = reverse_lazy('UserAdministration:ClientDeleteView', kwargs={'pk': self.first_c.pk})
 
     def test_delete_client(self):
         self.first_c.createdBy = self.user
         self.first_c.save()
-        self.client.post('/clients/{0}/delete'.format(self.first_c.pk))
+        self.client.post(self.url)
         clients = Clients.objects.all()
         self.assertEqual(
             clients.count(),
@@ -156,7 +168,7 @@ class ClientDeleteViewTest(LoginSetup):
         )
 
     def test_forbidden_delete_client(self):
-        self.client.post('/clients/{0}/delete'.format(self.first_c.pk))
+        self.client.post(self.url)
         clients = Clients.objects.all()
         self.assertEqual(
             clients.count(),
