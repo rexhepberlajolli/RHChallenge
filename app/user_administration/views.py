5f4c125fa8e5dc8fc5db61f506c581e7edf7e852
rexhepberlajolli <rexhepberlajolli@gmail.com>
2017-10-21 14:43:27 +0200

    Add ClientCreateView and ClientListView

--- app/user_administration/views.py
+++ app/user_administration/views.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.views.generic import TemplateView, View
+from django.views.generic import TemplateView, CreateView, ListView
 import logging
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.shortcuts import HttpResponse
+from .models import Clients
+from .forms import ClientsForm
+import json
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+
 
 logger = logging.getLogger(__name__)
 
@@ -14,6 +20,32 @@ class LoginPage(TemplateView):
 
 
 @method_decorator(login_required, name='dispatch')
-class HomePage(View):
-    def get(self, request):
-        return HttpResponse("Home Page")
+class ClientCreateView(CreateView):
+    model = Clients
+    form_class = ClientsForm
+    success_url = reverse_lazy('UserAdministration:ClientListView')
+
+    def form_valid(self, form):
+        messages.add_message(self.request, messages.SUCCESS, "Client Created Successfully")
+        return super(ClientCreateView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        errors = json.loads(form.errors.as_json())
+        for error in errors:
+            for message in errors[error]:
+                messages.add_message(self.request, messages.ERROR, message['message'])
+        return redirect(self.success_url)
+
+
+@method_decorator(login_required, name='dispatch')
+class ClientListView(ListView):
+    model = Clients
+    template_name = 'user_administration/clients.html'
+    context_object_name = 'clients'
+    paginate_by = 10
+    ordering = ['id']
+
+    def get_context_data(self, **kwargs):
+        context = super(ClientListView, self).get_context_data(**kwargs)
+        context['create_form'] = ClientsForm()
+        return context
