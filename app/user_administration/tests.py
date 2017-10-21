999752ec378bbf6d3017f7afc964090c6871b7d4
rexhepberlajolli <rexhepberlajolli@gmail.com>
2017-10-21 14:41:10 +0200

    Add TestCase for ClientListView and ClientCreateView

--- app/user_administration/tests.py
+++ app/user_administration/tests.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Clients
 
 
 class LoginRequiredTest(TestCase):
@@ -15,4 +17,47 @@ class LoginRequiredTest(TestCase):
             response.url,
             '/login?next=/',
             msg="Login Required Redirection Failed, Received url {0} instead of /login?next=/".format(response.url)
+        )
+
+
+class LoginSetup(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testUser', is_active=True, is_superuser=True)
+        self.user.set_password('RHChallenge')
+        self.user.save()
+        self.client.force_login(self.user)
+
+
+class ClientsViewTest(LoginSetup):
+    def setUp(self):
+        super(ClientsViewTest, self).setUp()
+        self.custom_client = Clients.objects.create(first_name='RH', last_name='CH', iban='IBAN')
+
+    def test_client_create(self):
+        data = {'first_name': 'Rexhep', 'last_name': 'Berlajolli', 'iban': 'XK051506001004471930'}
+        self.client.post('/add', data=data)
+        clients_count = Clients.objects.count()
+        self.assertEqual(
+            clients_count,
+            2,
+            msg="Create client failed, received {0} clients instead of 2".format(clients_count)
+        )
+
+    def test_client_create_validation(self):
+        data = {'first_name': 'Invalid', 'last_name': 'Data', 'iban': 'INVALID_IBAN'}
+        self.client.post('/add', data=data)
+        clients_count = Clients.objects.count()
+        self.assertEqual(
+            clients_count,
+            1,
+            msg="Insertion of invalid data succeeded, received {0} clients instead of 1".format(clients_count)
+        )
+
+    def test_get_clients(self):
+        response = self.client.get('/')
+        clients = response.context_data['clients']
+        self.assertEqual(
+            list(clients),
+            list(Clients.objects.all()),
+            msg="Get clients failed, received clients {0} instead of {1}".format(clients, [self.custom_client])
         )
