556ba8a362affd0dd8dcf666f7d21678c01bb4da
rexhepberlajolli <rexhepberlajolli@gmail.com>
2017-10-21 17:35:39 +0200

    Add routing for ClientUpdateView

--- app/user_administration/urls.py
+++ app/user_administration/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
-from .views import LoginPage, ClientCreateView, ClientListView, ClientDetailView
+from .views import LoginPage, ClientCreateView, ClientListView, ClientDetailView, ClientUpdateView
 from django.contrib.auth.views import logout_then_login
 
 urlpatterns = [
     url(r'^$', ClientListView.as_view(), name='ClientListView'),
     url(r'^add$', ClientCreateView.as_view(), name='ClientCreateView'),
-    url(r'^clients/(?P<pk>[^/]+)/', ClientDetailView.as_view(), name='ClientDetailView'),
+    url(r'^clients/(?P<pk>[^/]+)$', ClientDetailView.as_view(), name='ClientDetailView'),
+    url(r'^clients/(?P<pk>[^/]+)/edit$', ClientUpdateView.as_view(), name='ClientUpdateView'),
     url(r'^login/', LoginPage.as_view(), name='LoginPage'),
     url(r'^logout/', logout_then_login, name='LogoutPage'),
 ]
