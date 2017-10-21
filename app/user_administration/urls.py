2ac4a26c06550dda00ff0ed880abda3e1b3d1761
rexhepberlajolli <rexhepberlajolli@gmail.com>
2017-10-21 18:15:25 +0200

    Add routing for ClientDeleteView

--- app/user_administration/urls.py
+++ app/user_administration/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import LoginPage, ClientCreateView, ClientListView, ClientDetailView, ClientUpdateView
+from .views import LoginPage, ClientCreateView, ClientListView, ClientDetailView, ClientUpdateView, ClientDeleteView
 from django.contrib.auth.views import logout_then_login
 
 urlpatterns = [
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^add$', ClientCreateView.as_view(), name='ClientCreateView'),
     url(r'^clients/(?P<pk>[^/]+)$', ClientDetailView.as_view(), name='ClientDetailView'),
     url(r'^clients/(?P<pk>[^/]+)/edit$', ClientUpdateView.as_view(), name='ClientUpdateView'),
+    url(r'^clients/(?P<pk>[^/]+)/delete', ClientDeleteView.as_view(), name='ClientDeleteView'),
     url(r'^login/', LoginPage.as_view(), name='LoginPage'),
     url(r'^logout/', logout_then_login, name='LogoutPage'),
 ]
